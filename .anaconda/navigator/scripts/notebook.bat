f5e26f432c7db7955be7c19ec632e567647fd60f
RitheshSB <rithesh1010@gmail.com>
2018-05-28 22:55:52 +0530

    Including the Jupyter Notebooks

---
+++ .anaconda/navigator/scripts/notebook.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\Rithesh\Anaconda2\Scripts\activate C:\Users\Rithesh\Anaconda2
+C:\Users\Rithesh\Anaconda2\Scripts\jupyter-notebook >C:\Users\Rithesh\.anaconda\navigator\scripts\notebook-out-1.txt 2>C:\Users\Rithesh\.anaconda\navigator\scripts\notebook-err-1.txt
